a1e4dfde81b10a95d02850075b688f6f35f0f74a
Davide <davide@hibernate.org>
2022-06-19 09:36:40 +0100

    Test reactive code outside a tx

--- hibernate-reactive-panache-quickstart/src/test/java/org/acme/hibernate/orm/panache/ExamplePanacheTest.java
+++ hibernate-reactive-panache-quickstart/src/test/java/org/acme/hibernate/orm/panache/ExamplePanacheTest.java
@@ -1,22 +1,37 @@
 package org.acme.hibernate.orm.panache;
 
 import java.util.List;
+import javax.inject.Inject;
 
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.hibernate.reactive.panache.Panache;
+import io.quarkus.test.TestReactiveTransaction;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.vertx.RunOnVertxContext;
+import io.quarkus.test.vertx.UniAsserter;
+import io.smallrye.mutiny.Uni;
+import io.vertx.core.Vertx;
 import org.assertj.core.api.Assertions;
 
 @QuarkusTest
 public class ExamplePanacheTest {
 
 	@Test
-	public void test() {
+	@RunOnVertxContext
+	public void test(UniAsserter asserter) {
+		Uni<List<Fruit>> listAllUni = Fruit.<Fruit>listAll();
 		Fruit mandarino = new Fruit( "Mandarino" );
-		Panache.withTransaction( () -> Fruit.persist( mandarino ) ).await().indefinitely();
-		List<Fruit> result = Fruit.<Fruit>listAll().await().indefinitely();
-		Assertions.assertThat( result ).hasSize( 4 );
-		Assertions.assertThat( result ).contains( mandarino );
+		asserter.assertThat(
+				() -> Panache.withTransaction( () -> Fruit.persist( mandarino ).replaceWith( listAllUni ) ),
+				result -> {
+					Assertions.assertThat( result ).hasSize( 4 );
+					Assertions.assertThat( result ).contains( mandarino );
+				}
+		);
+	}
+
+	private void printThread(String step) {
+		System.out.println( step + " - " + Thread.currentThread().getId() + ":" + Thread.currentThread().getName() );
 	}
 }
